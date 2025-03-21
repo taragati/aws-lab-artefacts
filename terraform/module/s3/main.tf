e190c2dea728b8d422a7c67a7f5e77f7634cf3d1
taragati.ai <admin@niruzcorp.com>
2025-03-21 16:55:53 +0530

    Update main.tf

--- terraform/module/s3/main.tf
+++ terraform/module/s3/main.tf
@@ -1,3 +1,18 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0.0"
+      }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = "your_aws_access_key"
+    secret_key = "your_aws_secret_access_key"
+}
+
 resource "aws_s3_bucket" "example" {
   bucket = "niranjan-123234242"
 
