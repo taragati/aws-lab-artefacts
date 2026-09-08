3e18d2ac4a86170de90a9df735bd9c99978ab239
taragati.com <admin@niruzcorp.com>
2026-09-08 10:56:47 +0530

    Update DynamoDB table name and message payload

--- cloud-services/cloudwatch/lambda-dynamodb-traffic.py
+++ cloud-services/cloudwatch/lambda-dynamodb-traffic.py
@@ -3,22 +3,29 @@ import uuid
 
 dynamodb = boto3.resource("dynamodb")
 
-TABLE_NAME = "YOUR_TABLE_NAME"
+TABLE_NAME = "niranjan_table"
 table = dynamodb.Table(TABLE_NAME)
 
+# ~2 KB payload
+MESSAGE = "%TRAINING%" * 2000
+
+
 def lambda_handler(event, context):
+
     item_id = str(uuid.uuid4())
 
-    # 2 writes → approximately 2 WCU
+    # 2 writes
+    # Each item is > 1 KB, so each write consumes ~2 WCU
     for i in range(2):
         table.put_item(
             Item={
                 "id": f"{item_id}-{i}",
-                "message": "CloudWatch RCU/WCU Lab"
+                "message": MESSAGE
             }
         )
 
-    # 2 strongly consistent reads → approximately 2 RCU
+    # 2 strongly consistent reads
+    # Each item is < 4 KB, so each read consumes ~1 RCU
     for i in range(2):
         table.get_item(
             Key={
